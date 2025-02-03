8983b1d8172f1bb51770e7f1bc7729468637b14a
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-02-02 22:50:36 -0300

    refactor: add radix and type element root

--- mechanic-app-react/src/main.tsx
+++ mechanic-app-react/src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createRoot } from 'react-dom/client'
 import { App } from './app.tsx'
 import './index.css'
+import "@radix-ui/themes/styles.css";
 import { BrowserRouter } from 'react-router-dom'
 
 
@@ -13,7 +14,7 @@ googleMapsScript.async = true;
 document.head.appendChild(googleMapsScript);
 
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <QueryClientProvider client={client}>
       <App />
