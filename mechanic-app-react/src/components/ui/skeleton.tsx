163e9998afc860886361f4f049ca9df53ce784c6
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-02-14 22:18:12 -0300

    feat: add component ui skeleton loading

---
+++ mechanic-app-react/src/components/ui/skeleton.tsx
@@ -0,0 +1,17 @@
+import CarPNG from "../../../public/car.png"
+
+export function Skeleton() {
+    return (
+        <div className="max-w-sm animate-pulse gap-1.5 flex flex-col flex-1 ">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+            <img
+                className="flex rounded-md object-cover mt-6"
+                src={CarPNG} />
+        </div>
+    )
+}
